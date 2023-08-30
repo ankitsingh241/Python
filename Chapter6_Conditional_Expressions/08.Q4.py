d8815e1d88783f4ee5d29135fc5bb7818582f5c3
Ankit_Singh <iamankitsingh24@gmail.com>
2023-08-30 17:48:27 +0530

    Practice

--- Chapter6_Conditional_Expressions/08.Q4.py
+++ Chapter6_Conditional_Expressions/08.Q4.py
@@ -1,2 +1,12 @@
 # Write a program to find wheather a given username is less than 10 characters or not.
 
+name = input("Ener your name: ")
+length = len(name)
+if (length <= 10):
+    if(length == 10):
+        print(name + " is equal to 10 character.")
+    else:
+        print(name + " is less than 10 character.")
+else:
+    print(name + " is more than 10 character.")
+
