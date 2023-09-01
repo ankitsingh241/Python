c5937b012fe93ef7eb544d30f6569f9a6d2c45a7
Ankit_Singh <iamankitsingh24@gmail.com>
2023-09-01 13:38:53 +0530

    theory

---
+++ Chapter7_Loops/01.while_loop.py
@@ -0,0 +1,13 @@
+# To repeat a set of statements in our program is called loop.
+
+# If the loop is entered the process of condition check and excution
+# will be continued until the condition becomes false.
+
+# While loop
+
+i = 0
+while i<10:
+    print("OK " + str(i)) # If you run till here loop will continue to run.
+    i = i+1 # But if you add this loop will break at 10.
+
+print("Done")
