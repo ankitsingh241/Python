e784e99e4925fccf938e218be2d87a61848871b0
Ankit_Singh <iamankitsingh24@gmail.com>
2023-09-02 16:00:35 +0530

    Basics

---
+++ Chapter7_Loops/05.range_function.py
@@ -0,0 +1,13 @@
+# It is used to generate a sequence of numbers.
+# range(start, stop, step-size)
+
+for i in range(8):
+    print(i)        # It will print from 0 to 7.
+
+for i in range(1, 8):
+    print(i)        # It will print from 1 to 7.
+
+for i in range(1, 8, 2):
+    print(i)        # It will print after 2 steps.
+
+    
